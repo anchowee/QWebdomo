b78a4e6ea6b37505a002f7c1beee628bb2c63c04
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2013-05-06 21:52:15 +0200

    more debug

--- actuators/fake/QWFakeActuator.cpp
+++ actuators/fake/QWFakeActuator.cpp
@@ -1,4 +1,7 @@
 #include "QWFakeActuator.h"
+#ifdef QT_DEBUG
+#include <QDebug>
+#endif
 
 QWFakeActuator::QWFakeActuator(QObject *parent) :
     QWActuator(parent)
@@ -12,6 +15,9 @@ QList<QWAppliance> QWFakeActuator::put(QStringList &subtypes, QHash<QString, QVa
 
 QList<QWAppliance> QWFakeActuator::get(QStringList &subtypes, QHash<QString, QVariant> &attributes)
 {
+#ifdef QT_DEBUG
+    qDebug() << "do get";
+#endif
     QList<QWAppliance> apps = find(subtypes);
     for(int i = 0; i != apps.length(); i++){
         QHash<QString, QVariant>::const_iterator hi;
