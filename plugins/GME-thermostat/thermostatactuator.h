9510ef0e6547283f4d161e2842567b040c869cfe
Riccardo Ferrazzo <f.riccardo87@gmail.com>
2013-06-17 14:10:45 +0200

    added GME-thermostat plugin

---
+++ plugins/GME-thermostat/thermostatactuator.h
@@ -0,0 +1,27 @@
+#ifndef THERMOSTATACTUATOR_H
+#define THERMOSTATACTUATOR_H
+
+#include "QWActuator.h"
+#include <QtSerialPort/QSerialPort>
+
+class ThermostatActuator : public QWActuator
+{
+    Q_OBJECT
+public:
+    explicit ThermostatActuator(QObject *parent = 0);
+    ~ThermostatActuator();
+    
+    void initialize();
+    void get(const QString &senderJid, const QStringList &subtypes, const QHash<QString, QVariant> &attributes);
+    void changeState(QList<QSharedPointer<QWAppliance> > *selectedAppliances, const QHash<QString, QVariant> &newStates);
+
+private slots:
+    void getDeviceResponse();
+
+private:
+    QSerialPort *_serial;
+    qint8 _firstByte;
+    QString _lastSender; //FIXME: find a more reliable way to keep it in memory
+};
+
+#endif // THERMOSTATACTUATOR_H
